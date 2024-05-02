a73bc9d59abf908f6eff68395bf33cf9e21cb8b9
elsa <elsa@example.com>
2024-05-02 16:12:38 +0200

    fixed dataLoading and img

--- news-app/main.js
+++ news-app/main.js
@@ -4,12 +4,6 @@ const fetch = require('electron-fetch').default;
 
 config.config();
 
-let win;
-
-const API_KEY = process.env.APIKEY;
-const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-let articles = [];
-
 const createWindow = () => {
   win = new BrowserWindow({
     width: 800,
@@ -19,18 +13,6 @@ const createWindow = () => {
       win.loadFile('index.html')
     }
 
-const loadData = async () => {
-  try {
-    const res = await fetch(URL);
-    resJSON = await res.json();
-    articles = resJSON.articles
-  } catch {
-    console.error('Failed');
-  }
-  win.webContents.send('articlesData', articles);
-}
-
-loadData();
     
 app.whenReady().then(() => {
   createWindow()
--- news-app/renderer.js
+++ news-app/renderer.js
@@ -1,34 +1,45 @@
 const { ipcRenderer } = require('electron');
 const $ = require('jquery');
 
-let articles;
+const API_KEY = process.env.APIKEY;
+const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+let articles = [];
 
-const createArticlesList = () => {
-    //son image, son titre pointant vers l'URL de l'article, sa date de publication, son auteur et sa description
-    articles.map((article) => {
-        $('.containerArticles').append(`<li class="list-group-item">
-            <img src=${article.urlToImage}>
-            <a href=${article.url}>${article.title}</a>
-            <p> author:${article.author}</p>
-            <p>${article.publishedAt}</p>
-            <p>${article.description}</p>
-        </li>`);
-    })    
+function init(){
+    loadData();
+    createArticlesList();
+    $(".nav-group-item").on("click", handleClick);
 }
 
-ipcRenderer.on('articlesData', (event, data) => {
-    articles = data;
-    console.log(articles);
-    createArticlesList();
-})
+const loadData = async () => {
+    try {
+      const res = await fetch(URL);
+      resJSON = await res.json();
+      articles = resJSON.articles;
+      console.log(articles);
+    } catch {
+      console.error('Failed');
+    }
+}  
+
+const createArticlesList = () => {
+    articles.map((article) => {
+        $('.containerArticles').append(
+            `<li class="list-group-item">
+                <img class="media-object pull-left" src=${article.urlToImage}>
+                <a href=${article.url}>${article.title}</a>
+                <p> author:${article.author}</p>
+                <p>${article.publishedAt}.toLocaleDateString()</p>
+                <p>${article.description}</p>
+            </li>`
+        );
+    })    
+    $('img').css({'height': '6rem', 'width': '8rem'})
+}
 
 const handleClick = (event) => {
     $( ".nav-group-item" ).removeClass( "active" );
     $(event.target).addClass( "active" );
-}
-
-function init(){
-    $(".nav-group-item").on("click", handleClick);
 }
 
 init();
